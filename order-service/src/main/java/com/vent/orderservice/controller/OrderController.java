33c08c9136a44df0853329f8f79225a6908de05f
Ankit Sharma <sharmankit9593@gmail.com>
2023-02-02 15:06:04 +0530

    implemented @Retry using resilience4j

--- order-service/src/main/java/com/vent/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/vent/orderservice/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.vent.orderservice.controller;
 import com.vent.orderservice.dto.OrderRequest;
 import com.vent.orderservice.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,6 +20,7 @@ public class OrderController {
     @ResponseStatus(HttpStatus.CREATED)
     @CircuitBreaker(name="inventory", fallbackMethod = "fallbackMethod")
     @TimeLimiter(name = "inventory")
+    @Retry(name="inventory")
     public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
         return CompletableFuture.supplyAsync(()->orderService.placeOrder(orderRequest));
 //        return "Order placed successfully";
