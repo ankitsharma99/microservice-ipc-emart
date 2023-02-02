2a80f7f498bd14a3d981a1b820d4d92cf135ce0f
Ankit Sharma <sharmankit9593@gmail.com>
2023-02-02 14:35:20 +0530

    implemented circuitbreaker in order-service

--- order-service/src/main/java/com/vent/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/vent/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.vent.orderservice.controller;
 
 import com.vent.orderservice.dto.OrderRequest;
 import com.vent.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,8 +14,13 @@ public class OrderController {
     private final OrderService orderService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name="inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order placed successfully";
+    }
+
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return "Oops!! Something went wrong, please order after some time";
     }
 }
